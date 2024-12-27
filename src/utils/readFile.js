1e21db5c269a1d256c862cea1983c017853888b0
Nathan Healea <nate.healea@gmail.com>
2024-12-26 17:04:47 -0800

    Refactor the input file loader utilities

--- src/utils/readFile.js
+++ src/utils/readFile.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+export { fileURLToPath } from 'url';
 
 /**
  * Gets the contents of the AoC problem input.txt
@@ -7,22 +8,10 @@ import path from 'path';
  * @returns {string[]} file contents of the AoC problem input.txt
  */
 export const getAocInput = (cwd = '') => {
-  const filename = 'input.txt';
-  const filePath = path.resolve(cwd || path.dirname, filename);
-
-  if (!fs.existsSync(filePath)) {
-    console.error(`File ${filename} does not exists. \n${filePath}`);
-  }
-
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-
-  return fileContents.split(/\r\n/);
-};
-
-/**
- * Get the current working direction path.
- * @returns {string} path to the current working directory.
- */
-export const getCwd = () => {
-  return path.resolve(process.cwd());
+  const __dirname = path.dirname(cwd);
+  return fs
+    .readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8')
+    .trim()
+    .split('\n')
+    .map((line) => line.split(' ').map(Number));
 };
